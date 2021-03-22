f54ca34831ab30506999a5f0c97c32d61690a347
meAlexander <alexander.iv1998@gmail.com>
2021-03-22 16:14:08 +0200

    update ER Diagram

--- createTableQueries.sql
+++ createTableQueries.sql
@@ -6,40 +6,40 @@ CREATE TABLE users(
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(50) NOT NULL UNIQUE,
     age INT NOT NULL,
-    city VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
-    pass VARCHAR(100) NOT NULL,
-    register_date DATETIME NOT NULL DEFAULT now()
+    password VARCHAR(100) NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT now()
 );
 
 CREATE TABLE songs(
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    song_name VARCHAR(100) NOT NULL,
+    title VARCHAR(100) NOT NULL,
     views INT NOT NULL DEFAULT 0,
-    added_at DATETIME NOT NULL DEFAULT now(),
-    user_id INT NOT NULL,
-    FOREIGN KEY(user_id) REFERENCES users(id)
+    created_at DATETIME NOT NULL DEFAULT now(),
+    url VARCHAR(100) NOT NULL,
+    owner_id INT NOT NULL,
+    FOREIGN KEY(owner_id) REFERENCES users(id)
 );
 
 CREATE TABLE playlists(
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	playlist_name VARCHAR(50) NOT NULL,
-    user_id INT NOT NULL,
-    FOREIGN KEY(user_id) REFERENCES users(id),
+	name VARCHAR(50) NOT NULL,
+    owner_id INT NOT NULL,
+    FOREIGN KEY(owner_id) REFERENCES users(id),
     created_at DATETIME NOT NULL DEFAULT now()
 );
 
 CREATE TABLE comments(
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    comment_text TEXT NOT NULL,
-    user_id INT NOT NULL,
-    FOREIGN KEY(user_id) REFERENCES users(id),
+    text TEXT NOT NULL,
+    owner_id INT NOT NULL,
+    FOREIGN KEY(owner_id) REFERENCES users(id),
     song_id INT NOT NULL,
     FOREIGN KEY(song_id) REFERENCES songs(id),
     created_at DATETIME NOT NULL DEFAULT now()
 );
 
-CREATE TABLE songs_have_likes(
+CREATE TABLE users_like_songs(
 	song_id INT NOT NULL,
     FOREIGN KEY(song_id) REFERENCES songs(id),
     user_id INT NOT NULL,
@@ -47,7 +47,7 @@ CREATE TABLE songs_have_likes(
     PRIMARY KEY(song_id, user_id)
 );
 
-CREATE TABLE songs_have_dislikes(
+CREATE TABLE users_dislike_songs(
 	song_id INT NOT NULL,
     FOREIGN KEY(song_id) REFERENCES songs(id),
     user_id INT NOT NULL,
@@ -55,7 +55,7 @@ CREATE TABLE songs_have_dislikes(
     PRIMARY KEY(song_id, user_id)
 );
 
-CREATE TABLE comments_have_likes(
+CREATE TABLE users_like_comments(
 	comment_id INT NOT NULL,
     FOREIGN KEY(comment_id) REFERENCES comments(id),
     user_id INT NOT NULL,
@@ -63,7 +63,7 @@ CREATE TABLE comments_have_likes(
     PRIMARY KEY(comment_id, user_id)
 );
 
-CREATE TABLE comments_have_dislikes(
+CREATE TABLE users_dislike_comments(
 	comment_id INT NOT NULL,
     FOREIGN KEY(comment_id) REFERENCES comments(id),
     user_id INT NOT NULL,
@@ -79,16 +79,10 @@ CREATE TABLE playlists_have_songs(
     PRIMARY KEY(playlist_id, song_id)
 );
 
-CREATE TABLE users_have_followers(
-	user_id INT NOT NULL,
-    FOREIGN KEY(user_id) REFERENCES users(id),
-    following_user_id INT NOT NULL,
-    FOREIGN KEY(following_user_id) REFERENCES users(id),
-    PRIMARY KEY(user_id, following_user_id)
-);
-
-CREATE TABLE songs_have_values(
-	song_id INT NOT NULL,
-    FOREIGN KEY(song_id) REFERENCES songs(id),
-    song_value BLOB NOT NULL
+CREATE TABLE users_follow_users(
+	followed_id INT NOT NULL,
+    FOREIGN KEY(followed_id) REFERENCES users(id),
+    follower_id INT NOT NULL,
+    FOREIGN KEY(follower_id) REFERENCES users(id),
+    PRIMARY KEY(followed_id, follower_id)
 );
